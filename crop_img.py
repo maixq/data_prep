28b4e6a0e29038fc20382a2aa30b9efc74e654ef
maixq <mai.xueqiao@carro.co>
2023-01-31 07:50:55 +0000

    update dir changes

--- crop_img.py
+++ crop_img.py
@@ -1,8 +1,7 @@
 import numpy as np
 from PIL import Image
-import cv2
-import math 
-from models import panel_detector
+from detectors import panel_detector
+from dictionary import panel_classes
 
 class Cropper:
     
@@ -18,6 +17,7 @@ class Cropper:
 
         for box_idx in range(len(panel_boxes.tolist())):
             crop_panel = [k for k,v in self.class_d.items() if v == pred_classes[box_idx]][0]
+            # filter out headlight and license plate 
             if (crop_panel.split('_')[-1] == 'headlight') or (crop_panel.split('_')[-1] == 'plate') :
                 pass
             else:
@@ -25,43 +25,9 @@ class Cropper:
                 crop_img = Image.fromarray(self.image).crop((crop_pts[0], crop_pts[1], crop_pts[2], crop_pts[3]))
                 print('IMG SIZE: ', crop_img.size)
                 crop_img.save(self.dst+'{}_{}'.format(crop_panel, self.img_name))
-                # crop_img = cv2.cvtColor(np.array(crop_img), cv2.COLOR_BGR2RGB)
-                # print(crop_panel)
-                # Image.fromarray(crop_img).save(self.dst+'{}_{}'.format(crop_panel, self.img_name))
         return
 
 if __name__ == "__main__":
-    panel_classes = {
-                'car_license_plate': 0,
-                'front_bonnet': 1,
-                'front_bumper': 2,
-                'front_left_bonnet': 3,
-                'front_left_bumper': 4,
-                'front_left_door': 5,
-                'front_left_fender': 6,
-                'front_left_headlight': 7,
-                'front_left_side_mirror': 8,
-                'front_right_bonnet': 9,
-                'front_right_bumper': 10,
-                'front_right_door': 11,
-                'front_right_fender': 12,
-                'front_right_headlight': 13,
-                'front_right_side_mirror': 14,
-                'rear_bonnet': 15,
-                'rear_bumper': 16,
-                'rear_left_bonnet': 17,
-                'rear_left_bumper': 18,
-                'rear_left_door': 19,
-                'rear_left_fender': 20,
-                'rear_left_headlight': 21,
-                'rear_left_side_panel': 22,
-                'rear_right_bonnet': 23,
-                'rear_right_bumper': 24,
-                'rear_right_door': 25,
-                'rear_right_fender': 26,
-                'rear_right_headlight': 27,
-                'rear_right_side_panel': 28
-            }
 
     img_path = '/home/ubuntu/Users/maixueqiao/damage_detection/jeff/detectron2/projects/PointRend/data/images/val/carro_0tBHrYmBkkAn8HzI.jpg'
     dst = '/home/ubuntu/Users/maixueqiao/data_prep/cropped_img/'
