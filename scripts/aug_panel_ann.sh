57b7ba4d7b0802dfc646bb93452abb6738ad058d
maixq <mai.xueqiao@carro.co>
2023-02-02 07:03:57 +0000

    add scripts

---
+++ scripts/aug_panel_ann.sh
@@ -0,0 +1,4 @@
+python ../crop_img.py \
+	--dst /home/ubuntu/Users/maixueqiao/data_prep/test \
+    --img_source /home/ubuntu/Users/maixueqiao/data_prep/images \
+    --json_source /home/ubuntu/Users/maixueqiao/data_prep/train_coco.json
