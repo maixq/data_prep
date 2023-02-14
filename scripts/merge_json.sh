516672dab3edb3646bf7ffea2f5751d86f6e2894
maixq <mai.xueqiao@carro.co>
2023-02-14 10:13:17 +0000

    add script to merge json

---
+++ scripts/merge_json.sh
@@ -0,0 +1,4 @@
+python ../merge_json.py \
+    /home/ubuntu/Users/maixueqiao/data_prep/test/train-w-neg.json \
+    /home/ubuntu/Users/maixueqiao/data_prep/test/filter_new.json \
+    /home/ubuntu/Users/maixueqiao/data_prep/test/new_combined.json
